3fdcdec73be21a087c164c9ce832dac2581a99f7
João Furtado <jllf.17@gmail.com>
2022-09-19 09:19:35 +0100

    Added ci workflow

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+VERSION=$(npm pkg get version | tr -d '"')
 npx prisma generate
 npm run build
-docker buildx build --platform linux/arm64 --platform linux/amd64 --push -t shadept/novanuance:latest -t shadept/novanuance:1.3.1 .
-# docker run -it --rm -e DATABASE_URL=postgres://shade:Blac0po1n@192.168.1.200:5432/novanuance -p 3000:3000 shadept/novanuance:1.3.1
+docker buildx build --platform linux/arm64 --platform linux/amd64 --push -t shadept/novanuance:latest -t shadept/novanuance:$VERSION .
+# docker run -it --rm -e DATABASE_URL=postgres://shade:Blac0po1n@192.168.1.200:5432/novanuance -p 3000:3000 shadept/novanuance:$VERSION
