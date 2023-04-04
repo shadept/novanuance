3547df5e468d5b7e4d92016dd4ea0e9b91175fdb
João Furtado <jllf.17@gmail.com>
2023-04-04 10:59:28 +0100

    Maybe fix prisma binary

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -17,4 +17,14 @@ export const createContext = (opts: trpcNext.CreateNextContextOptions) => {
 
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
 
-export const createRouter = () => trpc.router<Context>();
+export const createRouter = () => trpc.router<Context>()
+  .middleware(async ({ path, type, next }) => {
+    const start = Date.now();
+    const result = await next();
+    const durationMs = Date.now() - start;
+    result.ok
+      ? console.log("OK request timing:", { path, type, durationMs })
+      : console.log("Non-OK request timing:", { path, type, durationMs, error: result.error })
+
+    return result;
+  });
