b31dbe1108d7de6142a0794309b3091faece4b46
João Furtado <jllf.17@gmail.com>
2023-04-04 09:48:47 +0100

    Fix daylight time savings issue

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
 npx prisma generate
 npm run build
-docker buildx build --platform linux/arm64 --platform linux/amd64 --push -t shadept/novanuance:latest -t shadept/novanuance:1.4.4 .
-@rem docker run -it --rm -e DATABASE_URL=postgres://shade:Blac0po1n@192.168.1.200:5432/novanuance -p 3000:3000 shadept/novanuance:1.4.4
+docker buildx build --platform linux/arm64 --platform linux/amd64 --push -t shadept/novanuance:latest -t shadept/novanuance:1.4.10 .
+@rem docker run -it --rm -e DATABASE_URL=postgres://shade:Blac0po1n@192.168.1.200:5432/novanuance -p 3000:3000 shadept/novanuance:1.4.10
